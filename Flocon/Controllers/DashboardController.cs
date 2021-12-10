f03a5036489f49ab5c02ad589aea7174fbf6710a
julienbeaudaux <julienbeaudaux@gmail.com>
2021-12-10 14:08:44 +0100

    Scaffolding

---
+++ Flocon/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Flocon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Flocon.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public DashboardController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
