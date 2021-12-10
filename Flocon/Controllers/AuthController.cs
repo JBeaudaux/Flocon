725724c9314d13f0dbc9069a967a0612cf54baa9
julienbeaudaux <julienbeaudaux@gmail.com>
2021-12-10 15:06:20 +0100

    Auth scaffold creation

---
+++ Flocon/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Flocon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Flocon.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AuthController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        /*
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        */
+    }
+}
