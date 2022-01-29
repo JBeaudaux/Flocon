8e51e8eee878441966143cffb1b162298bf30d30
Valentin Besse <valentinbesse@aumbox.net>
2022-01-29 15:42:34 +0100

    Set string.empty by default.

--- Flocon/Models/Auth/LoginViewModel.cs
+++ Flocon/Models/Auth/LoginViewModel.cs
@@ -5,10 +5,10 @@ namespace Flocon.Models.Auth
     public class LoginViewModel
     {
         /* -------------- DATA MANAGEMENT -------------- */
-        public string MetamaskAddr { get; set; } = string.Empty;
+        public string? MetamaskAddr { get; set; } = string.Empty;
 
         /* -------------- FORMS MODELS -------------- */
-        public string LoginEmail { get; set; } = string.Empty;
-        public string LoginPwd { get; set; } = string.Empty;
+        public string? LoginEmail { get; set; } = string.Empty;
+        public string? LoginPwd { get; set; } = string.Empty;
     }
 }
