33c917e95167b0ffb8b0addc16995d1a503527c5
Valentin Besse <valentinbesse@aumbox.net>
2022-01-29 01:46:18 +0100

    Initialized value to string.empty

--- Flocon/Models/Auth/LoginViewModel.cs
+++ Flocon/Models/Auth/LoginViewModel.cs
@@ -5,11 +5,10 @@ namespace Flocon.Models.Auth
     public class LoginViewModel
     {
         /* -------------- DATA MANAGEMENT -------------- */
-        public string MetamaskAddr { get; set; }
+        public string MetamaskAddr { get; set; } = string.Empty;
 
         /* -------------- FORMS MODELS -------------- */
-        public string LoginEmail { get; set; }
-        public string LoginPwd { get; set; }
-
+        public string LoginEmail { get; set; } = string.Empty;
+        public string LoginPwd { get; set; } = string.Empty;
     }
 }
